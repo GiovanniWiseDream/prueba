0358a5cfb5f9f019c26d476c7c3977d96fa1420a
GiovanniWiseDream <giovanni@thewisedreams.com>
2023-09-22 15:12:10 +0100

    360

--- src/components/ThreeScene.js
+++ src/components/ThreeScene.js
@@ -1,11 +1,11 @@
-import React from "react"; // Import missing hooks
 import { Canvas } from "@react-three/fiber"; // Import missing functions
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 
 const ThreeScene = ({ children }) => {
   return (
-    <Canvas>
+    <Canvas camera={{ position: [0, 0, 0.0000001] }}>
       {children} <OrbitControls />
+      <PerspectiveCamera />
     </Canvas>
   );
 };
